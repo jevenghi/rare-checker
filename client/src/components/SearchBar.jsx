66d917d6d2af3724d51fa829c5082cff8f9f0715
Jev Lap <jevenghi@gmail.com>
2025-04-12 07:16:32 +0200

    clear button position adjusted

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -27,8 +27,8 @@ function SearchBar({ getReleaseStats, query, setQuery, loading }) {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
+              width="25"
+              height="25"
               viewBox="0 0 24 24"
               fill="none"
               stroke="red"
